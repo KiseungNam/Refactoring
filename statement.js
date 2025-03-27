65f4b316c6d8b02cff83939cdf63dd0d833a1ee8
david.nam <david.nam@pulmuone.com>
2025-03-27 22:05:34 +0900

    totalAmount 함수 추출

--- statement.js
+++ statement.js
@@ -6,15 +6,23 @@ function statement(invoice, plays) {
     let result = `청구 내역 (고객명: ${invoice.customer})\n`;
     
     for (let perf of invoice.performances) {
-                
         // 청구내역 출력 줄 추가
         result += `  ${playFor(perf).name}: ${usd(amountFor(perf))}원 (${perf.audience}석)\n`;
-        totalAmount += amountFor(perf);
     }
+    totalAmount = appleSauce(invoice);
     
     result += `총액: ${usd(totalAmount)}원\n`;
     result += `적립 포인트: ${totalVolumeCredits(invoice)}점\n`;
     return result;
+}
+
+function appleSauce(invoice){
+    let totalAmount = 0;
+    
+    for (let perf of invoice.performances) {
+        totalAmount += amountFor(perf);
+    }
+    return totalAmount;
 }
 
 function totalVolumeCredits(invoice){
