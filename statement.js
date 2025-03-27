5fdf4bcddbdc87e5fb887589421ce3cbf526dc82
david.nam <david.nam@pulmuone.com>
2025-03-27 22:27:57 +0900

    playFor() 호출부분 중간데이터 사용으로 변경

--- statement.js
+++ statement.js
@@ -9,7 +9,12 @@ function statement(invoice, plays) {
 
     function enrichPerformance(aPerformance){
         const result = Object.assign({}, aPerformance); // 얕은 복사
+        result.play = playFor(result);  // 중간 데이터에 연극 정보를 저장
         return result;
+    }
+
+    function playFor(aPerformance){
+        return plays[aPerformance.playID];
     }
 }
 
@@ -17,7 +22,7 @@ function renderPlainText(data, plays){
     let result = `청구 내역 (고객명: ${data.customer})\n`;
     
     for (let perf of data.performances) {
-        result += `  ${playFor(perf).name}: ${usd(amountFor(perf))}원 (${perf.audience}석)\n`;
+        result += `  ${perf.play.name}: ${usd(amountFor(perf))}원 (${perf.audience}석)\n`;
     }
 
     result += `총액: ${usd(totalAmount(data))}원\n`;
@@ -58,21 +63,17 @@ function renderPlainText(data, plays){
         let result = 0;
         result += Math.max(aPerformance.audience - 30, 0);
         
-        if ("comedy" === playFor(aPerformance).type) {
+        if ("comedy" === aPerformance.play.type) {
             result += Math.floor(aPerformance.audience / 5);
         }
         return result;
-    }
-    
-    function playFor(aPerformance){
-        return plays[aPerformance.playID];
     }
     
     // 값이 바뀌지 않는 변수는 매개변수로 전달
     function amountFor(aPerformance){
         let result = 0;     // 변수를 초기화하는 코드
     
-        switch (playFor(aPerformance).type) {
+        switch (aPerformance.play.type) {
             case "tragedy":     // 비극
                 result = 40000;
                 if (aPerformance.audience > 30) {
@@ -87,7 +88,7 @@ function renderPlainText(data, plays){
                 result += 300 * aPerformance.audience;
                 break;
             default:
-                throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
+                throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
         }
         return result;      // 함수 안에서 값이 바뀌는 변수 반환
     }
