94e5408e2daa307ca2fc453f16f80729db160aa7
david.nam <david.nam@pulmuone.com>
2025-03-27 22:12:08 +0900

    statement 전체를 renderPlainText 함수로 추출

--- statement.js
+++ statement.js
@@ -2,6 +2,10 @@ const invoice = require('./invoices.json');
 const plays = require('./plays.json');
 
 function statement(invoice, plays) {
+    return renderPlanText(invoice, plays);
+}
+
+function renderPlanText(invoice, plays){
     let result = `청구 내역 (고객명: ${invoice.customer})\n`;
     
     for (let perf of invoice.performances) {
@@ -11,74 +15,76 @@ function statement(invoice, plays) {
     result += `총액: ${usd(totalAmount(invoice))}원\n`;
     result += `적립 포인트: ${totalVolumeCredits(invoice)}점\n`;
     return result;
-}
 
-function totalAmount(invoice){
-    let result = 0;
+
+    function totalAmount(invoice){
+        let result = 0;
+        
+        for (let perf of invoice.performances) {
+            result += amountFor(perf);
+        }
+        return result;
+    }
     
-    for (let perf of invoice.performances) {
-        result += amountFor(perf);
+    function totalVolumeCredits(invoice){
+        let result = 0;
+        // volumeCredits 별도 for문으로 분리
+        for (let perf of invoice.performances) {
+           // 포인트 적립
+           result += volumeCreditsFor(perf);    // 추출한 함수로 값을 누적 처리
+        }
+        return result;
     }
-    return result;
-}
-
-function totalVolumeCredits(invoice){
-    let result = 0;
-    // volumeCredits 별도 for문으로 분리
-    for (let perf of invoice.performances) {
-       // 포인트 적립
-       result += volumeCreditsFor(perf);    // 추출한 함수로 값을 누적 처리
-    }
-    return result;
-}
-
-// format 함수 추출
-function usd(aNumber){
-    return new Intl.NumberFormat("en-US", 
-                    {style:"currency"
-                        , currency: "USD"
-                        , minimumFractionDigits:2}
-                    ).format(aNumber/100);
-}
-
-// volumeCredits 함수 추출
-function volumeCreditsFor(aPerformance){
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
     
-    if ("comedy" === playFor(aPerformance).type) {
-        result += Math.floor(aPerformance.audience / 5);
+    // format 함수 추출
+    function usd(aNumber){
+        return new Intl.NumberFormat("en-US", 
+                        {style:"currency"
+                            , currency: "USD"
+                            , minimumFractionDigits:2}
+                        ).format(aNumber/100);
     }
-    return result;
-}
-
-// 값이 바뀌지 않는 변수는 매개변수로 전달
-function amountFor(aPerformance){
-    let result = 0;     // 변수를 초기화하는 코드
-
-    switch (playFor(aPerformance).type) {
-        case "tragedy":     // 비극
-            result = 40000;
-            if (aPerformance.audience > 30) {
-            result += 1000 * (aPerformance.audience - 30);
-            }
-            break;
-        case "comedy":      // 희극
-            result = 30000;
-            if (aPerformance.audience > 20) {
-            result += 10000 + 500 * (aPerformance.audience - 20);
-            }
-            result += 300 * aPerformance.audience;
-            break;
-        default:
-            throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
+    
+    // volumeCredits 함수 추출
+    function volumeCreditsFor(aPerformance){
+        let result = 0;
+        result += Math.max(aPerformance.audience - 30, 0);
+        
+        if ("comedy" === playFor(aPerformance).type) {
+            result += Math.floor(aPerformance.audience / 5);
+        }
+        return result;
     }
-    return result;      // 함수 안에서 값이 바뀌는 변수 반환
+    
+    function playFor(aPerformance){
+        return plays[aPerformance.playID];
+    }
+    
+    // 값이 바뀌지 않는 변수는 매개변수로 전달
+    function amountFor(aPerformance){
+        let result = 0;     // 변수를 초기화하는 코드
+    
+        switch (playFor(aPerformance).type) {
+            case "tragedy":     // 비극
+                result = 40000;
+                if (aPerformance.audience > 30) {
+                result += 1000 * (aPerformance.audience - 30);
+                }
+                break;
+            case "comedy":      // 희극
+                result = 30000;
+                if (aPerformance.audience > 20) {
+                result += 10000 + 500 * (aPerformance.audience - 20);
+                }
+                result += 300 * aPerformance.audience;
+                break;
+            default:
+                throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
+        }
+        return result;      // 함수 안에서 값이 바뀌는 변수 반환
+    }
 }
 
-function playFor(aPerformance){
-    return plays[aPerformance.playID];
-}
 
 module.exports = statement;
 
