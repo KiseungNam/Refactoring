a9d6b3b9cd10ab7bddcdae4f779ec5e966b0fad4
david.nam <david.nam@pulmuone.com>
2025-03-27 21:30:58 +0900

    perf -> aPerformance 변수명 변경

--- statement.js
+++ statement.js
@@ -30,22 +30,22 @@ function statement(invoice, plays) {
 }
 
 // 값이 바뀌지 않는 변수는 매개변수로 전달
-function amountFor(perf, play){
+function amountFor(aPerformance, play){
     let result = 0;     // 변수를 초기화하는 코드
 
     switch (play.type) {
         case "tragedy":     // 비극
             result = 40000;
-            if (perf.audience > 30) {
-            result += 1000 * (perf.audience - 30);
+            if (aPerformance.audience > 30) {
+            result += 1000 * (aPerformance.audience - 30);
             }
             break;
         case "comedy":      // 희극
             result = 30000;
-            if (perf.audience > 20) {
-            result += 10000 + 500 * (perf.audience - 20);
+            if (aPerformance.audience > 20) {
+            result += 10000 + 500 * (aPerformance.audience - 20);
             }
-            result += 300 * perf.audience;
+            result += 300 * aPerformance.audience;
             break;
         default:
             throw new Error(`알 수 없는 장르: ${play.type}`);
