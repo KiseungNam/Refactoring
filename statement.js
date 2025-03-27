79b36bdcdf07cd18dfcd4b50a750c532a959d01d
david.nam <david.nam@pulmuone.com>
2025-03-27 21:14:53 +0900

    명확한 변수명으로 변경

--- statement.js
+++ statement.js
@@ -8,7 +8,7 @@ function statement(invoice, plays) {
     
     for (let perf of invoice.performances) {
         const play = plays[perf.playID];
-        let thisAmount = amountFor(perf, play);     // 추출한 함수로 변환
+        let result = amountFor(perf, play);     // 추출한 함수로 변환
 
         // 포인트 적립
         volumeCredits = Math.max(perf.audience - 30, 0);
@@ -17,8 +17,8 @@ function statement(invoice, plays) {
         if (play.type === "comedy") volumeCredits += Math.floor(perf.audience / 5);
 
         // 출력 줄 추가
-        result += `  ${play.name}: ${(thisAmount / 100).toFixed(2)}원 (${perf.audience}석)\n`;
-        totalAmount += thisAmount;
+        result += `  ${play.name}: ${(result / 100).toFixed(2)}원 (${perf.audience}석)\n`;
+        totalAmount += result;
     }
 
     result += `총액: ${(totalAmount / 100).toFixed(2)}원\n`;
@@ -27,26 +27,26 @@ function statement(invoice, plays) {
 
 // 값이 바뀌지 않는 변수는 매개변수로 전달
 function amountFor(perf, play){
-    let thisAmount = 0;     // 변수를 초기화하는 코드
+    let result = 0;     // 변수를 초기화하는 코드
 
     switch (play.type) {
         case "tragedy":     // 비극
-            thisAmount = 40000;
+            result = 40000;
             if (perf.audience > 30) {
-            thisAmount += 1000 * (perf.audience - 30);
+            result += 1000 * (perf.audience - 30);
             }
             break;
         case "comedy":      // 희극
-            thisAmount = 30000;
+            result = 30000;
             if (perf.audience > 20) {
-            thisAmount += 10000 + 500 * (perf.audience - 20);
+            result += 10000 + 500 * (perf.audience - 20);
             }
-            thisAmount += 300 * perf.audience;
+            result += 300 * perf.audience;
             break;
         default:
             throw new Error(`알 수 없는 장르: ${play.type}`);
     }
-    return thisAmount;      // 함수 안에서 값이 바뀌는 변수 반환
+    return result;      // 함수 안에서 값이 바뀌는 변수 반환
 }
 
 module.exports = statement;
