310eec0277a86649a2d466bc447c0682f6b3d61e
david.nam <david.nam@pulmuone.com>
2025-03-27 21:28:56 +0900

    테스트케이스 및 소스 수정

--- statement.js
+++ statement.js
@@ -1,14 +1,17 @@
-const invoices = require('./invoices.json');
+const invoice = require('./invoices.json');
 const plays = require('./plays.json');
 
 function statement(invoice, plays) {
     let totalAmount = 0;
     let volumeCredits = 0;
     let result = `청구 내역 (고객명: ${invoice.customer})\n`;
-    
+    const format = new Intl.NumberFormat("en-US",
+            {style:"currency", currency: "USD"
+            , minimumFractionDigits:2}).format;
+
     for (let perf of invoice.performances) {
         const play = plays[perf.playID];
-        let result = amountFor(perf, play);     // 추출한 함수로 변환
+        let thisAmount = amountFor(perf, play);     // 추출한 함수로 변환
 
         // 포인트 적립
         volumeCredits = Math.max(perf.audience - 30, 0);
@@ -17,31 +20,32 @@ function statement(invoice, plays) {
         if (play.type === "comedy") volumeCredits += Math.floor(perf.audience / 5);
 
         // 출력 줄 추가
-        result += `  ${play.name}: ${(result / 100).toFixed(2)}원 (${perf.audience}석)\n`;
-        totalAmount += result;
+        result += `  ${play.name}: ${format(thisAmount / 100)}원 (${perf.audience}석)\n`;
+        totalAmount += thisAmount;
     }
 
-    result += `총액: ${(totalAmount / 100).toFixed(2)}원\n`;
+    result += `총액: ${format(totalAmount / 100)}원\n`;
+    result += `적립 포인트: ${volumeCredits}점\n`;
     return result;
 }
 
 // 값이 바뀌지 않는 변수는 매개변수로 전달
-function amountFor(aPerformance, play){
+function amountFor(perf, play){
     let result = 0;     // 변수를 초기화하는 코드
 
     switch (play.type) {
         case "tragedy":     // 비극
             result = 40000;
-            if (aPerformance.audience > 30) {
-            result += 1000 * (aPerformance.audience - 30);
+            if (perf.audience > 30) {
+            result += 1000 * (perf.audience - 30);
             }
             break;
         case "comedy":      // 희극
             result = 30000;
-            if (aPerformance.audience > 20) {
-            result += 10000 + 500 * (aPerformance.audience - 20);
+            if (perf.audience > 20) {
+            result += 10000 + 500 * (perf.audience - 20);
             }
-            result += 300 * aPerformance.audience;
+            result += 300 * perf.audience;
             break;
         default:
             throw new Error(`알 수 없는 장르: ${play.type}`);
@@ -52,6 +56,6 @@ function amountFor(aPerformance, play){
 module.exports = statement;
 
 // 실행
-console.log(statement(invoices[0], plays));
+console.log(statement(invoice[0], plays));
 
  
--- test.js
+++ test.js
@@ -6,10 +6,11 @@ const statement = require('./statement');  // 함수 분리해둔 경우
 
 const expectedOutput = 
 `청구 내역 (고객명: BigCo)
-  Hamlet: 650.00원 (55석)
-  As You Like It: 580.00원 (35석)
-  Othello: 500.00원 (40석)
-총액: 1730.00원
+  Hamlet: $650.00원 (55석)
+  As You Like It: $580.00원 (35석)
+  Othello: $500.00원 (40석)
+총액: $1,730.00원
+적립 포인트: 10점
 `;
 
 const actualOutput = statement(invoices[0], plays);
